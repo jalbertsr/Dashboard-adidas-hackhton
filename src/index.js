26e4eafb5e4d5e70b9e4640724ca407e6f898dc8
Joan <jalbertsr@protonmail.ch>
2018-07-02 23:29:07 +0200

    heroku deploy

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
 
-// import "assets/css/material-dashboard-react.css";
+import "assets/css/material-dashboard-react.css";
 
 import indexRoutes from "routes/index.jsx";
 
