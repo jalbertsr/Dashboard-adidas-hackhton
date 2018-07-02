d8afe641bba627412f9e08b135bc0fbf4db110cb
Joan <jalbertsr@protonmail.ch>
2018-07-02 12:05:53 +0200

    start dashboard

---
+++ src/routes/dashboard.jsx
@@ -0,0 +1,62 @@
+// @material-ui/icons
+import Dashboard from "@material-ui/icons/Dashboard";
+import Person from "@material-ui/icons/Person";
+import ContentPaste from "@material-ui/icons/ContentPaste";
+import LibraryBooks from "@material-ui/icons/LibraryBooks";
+import LocationOn from "@material-ui/icons/LocationOn";
+import Notifications from "@material-ui/icons/Notifications";
+// core components/views
+import DashboardPage from "views/Dashboard/Dashboard.jsx";
+import UserProfile from "views/UserProfile/UserProfile.jsx";
+import TableList from "views/TableList/TableList.jsx";
+import Typography from "views/Typography/Typography.jsx";
+import Maps from "views/Maps/Maps.jsx";
+import NotificationsPage from "views/Notifications/Notifications.jsx";
+
+const dashboardRoutes = [
+  // {
+  //   path: "/dashboard",
+  //   sidebarName: "Dashboard",
+  //   navbarName: "Material Dashboard",
+  //   icon: Dashboard,
+  //   component: DashboardPage
+  // },
+  {
+    path: "/user",
+    sidebarName: "User Profile",
+    navbarName: "Profile",
+    icon: Person,
+    component: UserProfile
+  },
+  {
+    path: "/table",
+    sidebarName: "Table List",
+    navbarName: "Table List",
+    icon: ContentPaste,
+    component: TableList
+  },
+  {
+    path: "/typography",
+    sidebarName: "Typography",
+    navbarName: "Typography",
+    icon: LibraryBooks,
+    component: Typography
+  },
+  {
+    path: "/maps",
+    sidebarName: "Maps",
+    navbarName: "Map",
+    icon: LocationOn,
+    component: Maps
+  },
+  {
+    path: "/notifications",
+    sidebarName: "Notifications",
+    navbarName: "Notifications",
+    icon: Notifications,
+    component: NotificationsPage
+  },
+  { redirect: true, path: "/", to: "/dashboard", navbarName: "Redirect" }
+];
+
+export default dashboardRoutes;
