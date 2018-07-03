2e846c32c95930a4c523dc7af22c9e13b04c4957
knowledge91 <dirkhornung91@gmail.com>
2018-07-03 06:43:08 +0200

    test

--- src/utils/contract.js
+++ src/utils/contract.js
@@ -1,6 +1,24 @@
 export const address = '0xdd8e48756ab62215b21ff118f70ebe5f91c053f5'
 export const adidasiumAddress = '0xbce7f803a5368050cc89976558b79c9980d947df'
-export const abi = [
+export const abi =[
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_dataScientistId",
+				"type": "string"
+			},
+			{
+				"name": "_dataholderId",
+				"type": "string"
+			}
+		],
+		"name": "addDataScientistToDataholder",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
 	{
 		"constant": false,
 		"inputs": [
@@ -32,15 +50,15 @@ export const abi = [
 		"constant": false,
 		"inputs": [
 			{
-				"name": "dataholderId",
+				"name": "_dataholderId",
 				"type": "string"
 			},
 			{
-				"name": "password",
+				"name": "_password",
 				"type": "string"
 			},
 			{
-				"name": "secretKey",
+				"name": "_privateKey",
 				"type": "string"
 			}
 		],
@@ -51,10 +69,124 @@ export const abi = [
 		"type": "function"
 	},
 	{
-		"inputs": [],
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_dataholderId",
+				"type": "string"
+			},
+			{
+				"name": "_dataScientistId",
+				"type": "string"
+			},
+			{
+				"name": "_buyerId",
+				"type": "string"
+			}
+		],
+		"name": "queryData",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_from",
+				"type": "string"
+			},
+			{
+				"name": "_to",
+				"type": "string"
+			},
+			{
+				"name": "_value",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"name": "coinbase",
+				"type": "string"
+			}
+		],
 		"payable": false,
 		"stateMutability": "nonpayable",
 		"type": "constructor"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "user",
+				"type": "string"
+			}
+		],
+		"name": "getBalanceOf",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "_dataholderId",
+				"type": "string"
+			}
+		],
+		"name": "getDataholder",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "_dataholderId",
+				"type": "string"
+			},
+			{
+				"name": "_dataScientistId",
+				"type": "string"
+			}
+		],
+		"name": "getKey",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
 	},
 	{
 		"constant": true,
@@ -75,4 +207,4 @@ export const abi = [
 		"stateMutability": "view",
 		"type": "function"
 	}
-]; 
+] ; 
